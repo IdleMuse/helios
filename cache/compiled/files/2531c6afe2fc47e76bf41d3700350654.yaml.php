69fb3afac1ce025b2c9a6d101d11dd4a993c32db
IdleMuse <idlemuse@ghostlit.co.uk>
2016-09-18 19:52:49 +0000

    removing certain dirs

--- cache/compiled/files/2531c6afe2fc47e76bf41d3700350654.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/public/user/config/streams.yaml',
-    'modified' => 1459706984,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/28c0198616cc326aaba3b7f047225487.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/public/user/plugins/devtools/devtools.yaml',
-    'modified' => 1474226834,
-    'data' => [
-        'enabled' => true
-    ]
-];
